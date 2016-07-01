fd4271b77207c9383e6089e10dc89a9da8e92602
Julio <jmcorral2@miners.utep.edu>
2016-07-01 16:53:01 -0600

    Identifed useful keys
    Splitting the output by every method in the .java file... so by each method (including main) it will output all its statments. If the method has a conditional OR iterative stmt.. it will ouput the following:
    
    Printing out the line of every conditional and iterative statment
    Identifyng where does conditiona/iterative starts and end
    Condition being evaluated in if and while statments so it might be possible to detect  && and ||

--- src/main/java/com/code/graph/CodeGraph.java
+++ src/main/java/com/code/graph/CodeGraph.java
@@ -2,16 +2,19 @@ package com.code.graph;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
 import java.io.*;
 import java.util.*;
 
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.BreakStmt;
 import com.github.javaparser.ast.stmt.ForStmt;
 import com.github.javaparser.ast.stmt.IfStmt;
 import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.stmt.WhileStmt;
 import com.github.javaparser.ast.visitor.GenericVisitor;
+import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
 import com.github.javaparser.ast.visitor.VoidVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.sun.glass.ui.EventLoop;
@@ -45,67 +48,95 @@ public class CodeGraph
         // System.out.println(cu.toString()); */
 
         System.out.println("Visiting Methods in FractionalToBinary.java");
+        System.out.println();
         new MethodVisitorTest().visit(cu, null);
+        
 
     }
 
-  
+   
+    private static class MethodGenericTest extends GenericVisitorAdapter{
+        
+      //whuuuut? genericvisitoradapat might store blocks in a list by defaul... 'might'"
+       
+    }
    
     
     /**
-     * Simple visitor implementation for visiting MethodDeclaration nodes.
+     * visiting methods using VoidVisitorAdapter parent class.
      */
     private static class MethodVisitorTest extends VoidVisitorAdapter {
 
+        
+//        public static void getAll_Methods(MethodDeclaration n, Object arg){
+//             BlockStmt list_of_methods = new BlockStmt();
+//             
+//        }
+        
         @Override
         public void visit(MethodDeclaration n, Object arg) {
-            // here you can access the attributes of the method.
-            // this method will be called for all methods in this
-            // CompilationUnit, including inner class methods
+            
 //            System.out.println(n.getName());//Gets method names.
 //            System.out.println(n.getBody().getChildrenNodes());//Gets contents of method.
 
             List<Statement> statements;
-            List<IfStmt> current_ifs;
-            List<ForStmt> current_fors;
+             BlockStmt block = new BlockStmt();
             
-           
+            
+//             
+//             List<IfStmt> if_statements_list;
+//            List<ForStmt> for_statements_list;
+//            IfStmt instance_IF = new IfStmt();
+//            ForStmt instance_FOR = new ForStmt();
 
             //Test to see if we can get contents of method and set them in a block.
-            System.out.println("----------------Statements are: -------------------");
+            System.out.println("----------------Statements in [" + n.getName() + "] are: ------------------------------------------------------------------");
+          
+        
             
-            BlockStmt block = new BlockStmt();
-            IfStmt conditional_IF = new IfStmt();
-            ForStmt iterative_FOR = new ForStmt();
             
             block = n.getBody();
             
             
             statements = block.getStmts();
             
+            
             int x =1;
             for(Statement statement: statements){
-                System.out.println("("+ x + ") ");
-                System.out.println(statement);
+
+                System.out.println("{"+ x + "} ");
+                
+//                if(statement instanceof IfStmt)
+//                {
+//                    System.out.println("This is an if_stmt!!->  "+statement.getChildrenNodes());
+//                }
+                System.out.println(statement.getChildrenNodes());
                 
                 x++;
             }
             System.out.println();
-             System.out.println("------------------------------------------------");
-            //Ok so, in Java blocks are executed as a single statement. That's why if you run this, you can see the statement separated by commas and a block is seen as a statement.
-            //What's a block in java? https://docs.oracle.com/javase/tutorial/java/nutsandbolts/expressions.html
-
-            super.visit(n, arg);//Makes a note of the methods visited by marking them through VoidVisitorAdapter
-
+            
+           
+             
+            super.visit(n, arg);
 
         }
         
+
+        
         @Override
         public void visit(IfStmt n, Object org){
-            
-            
-            System.out.println("CURRENT if CONDITIONS found--> "+n.getCondition());
-//            System.out.println("ALL IF data "+n.getData());
+           
+            if(n instanceof IfStmt){
+                System.out.println("IF-STMT at line ("+n.getBeginLine()+") --> "+n.getCondition());
+                if(n.getThenStmt() != null){
+                     System.out.println("THEN---STMT at line ("+n.getThenStmt().getBeginLine()+") --> "+n.getThenStmt());
+                }
+                if(n.getElseStmt() != null){
+                    System.out.println("ELSE---STMT at line("+n.getElseStmt().getBeginLine()+") --> "+n.getElseStmt());
+                }
+                System.out.println("IF-STMT ENDS at line ("+n.getEndLine()+")");
+            }
             super.visit(n,org);
             
         }
@@ -113,9 +144,13 @@ public class CodeGraph
         @Override
         public void visit(ForStmt n, Object org){
             
+             if(n instanceof ForStmt){
+                 
+                System.out.println("FOR-LOOP is at line--> ("+n.getBeginLine()+") --> "+n.getCompare());
+                System.out.println("FOR-LOOP BODY "+n.getBody());
+                System.out.println("FOR-LOOP ENDS at line ("+n.getEndLine()+")");
+             }
             
-            System.out.println("CONDITION in the FOR--> "+n.getCompare());
-            System.out.println("BODY inside each FORLOOP"+n.getBody());
             super.visit(n,org);
             
         }
@@ -123,16 +158,47 @@ public class CodeGraph
          @Override
         public void visit(WhileStmt n, Object org){
             
+            if(n instanceof WhileStmt){
+            System.out.println("WHILE-LOOP is --> ("+n.getBeginLine()+") --> "+n.getCondition());
+            System.out.println("WHILE-LOOP BODY is --> "+n.getBody());
+            System.out.println("WHILE-LOOP ENDS at line ("+n.getEndLine()+")");
+            }
             
-            System.out.println("CONDITION in the WHILE"+n.getCondition());
-            System.out.println("BODY inside each WHILELOOP "+n.getBody());
             super.visit(n,org);
             
         }
         
+         @Override
+        public void visit(BreakStmt n, Object org){
+            
+            if(n instanceof BreakStmt){
+            System.out.println("BREAK! is --> ("+n.getBeginLine()+") --> ID="+n.getId());
+           
+            }
+            
+            super.visit(n,org);
+            
+        }
+        
+        
+        
+        
+             //ignore this funciton for now, it might be usfule to read every single word in the code
+        public static void process(Node node){
+            System.out.println("Trigger process funtion!!!!!");
+            //this loop get a node, (a node is also know as a statment)
+            for(Node child : node.getChildrenNodes()){
+                System.out.println("My child is-----> "+child);
+                process(child);
+            }
+            
+            System.out.println("BEGIN:  "+node.getBeginLine());
+             System.out.println("END:   "+node.getEndLine());
+              System.out.println("PARENT NODE:   "+node.getParentNode());
+              System.out.println("-----");
+        }
+
+        
     }
-    
-    
-   
 
 }
