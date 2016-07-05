180d2fdf93c7f7f8494a9ee5415ff1d17273eaf1
Julio <jmcorral2@miners.utep.edu>
2016-07-05 17:24:28 -0600

    Statments for every method
    Count all statements
    Know where a for/if/while statement occrus: located its line of code and its statement Number

--- src/main/java/com/code/graph/CodeGraph.java
+++ src/main/java/com/code/graph/CodeGraph.java
@@ -21,98 +21,99 @@ import com.sun.glass.ui.EventLoop;
 import org.apache.commons.lang3.StringEscapeUtils;
 
 /**
- * @author
+ * @author Julio M. Corral
  *
  */
 public class CodeGraph
 {
+    
+    static  List<Statement> LIST_STATEMENTS;
+    //count statments from the .java input code
+    static int statmentID =1;
+    
     public static void main( String[] args ) throws IOException, ParseException {
 
-        String status = "Status: Testing method visitor.";
-        System.out.println(status);
+        System.out.println("Running--> Parse .java code to identify type of statments: sequencial, iterative, conditional");
 
-        // creates an input stream for the file to be parsed
-      
-         FileInputStream in = new FileInputStream("/Users/jmcorral2/Desktop/FractionalToBinary.java");
+       
+        // .java input code
+         FileInputStream in = new FileInputStream("/Users/jmcorral2/Desktop/QuickSort.java");
         CompilationUnit cu;
         try {
-            // parse the file
+            // parse .java input code
             cu = JavaParser.parse(in);
             
         } finally {
             in.close();
         }
 
-        /* prints the resulting compilation unit to default system output
-        // System.out.println("Printing out contents of class");
-        // System.out.println(cu.toString()); */
+        
 
-        System.out.println("Visiting Methods in FractionalToBinary.java");
+//        System.out.println(cu.toString());
+        System.out.println("Visiting Methods in QuickSort.java");
         System.out.println();
-        new MethodVisitorTest().visit(cu, null);
-        
+        new ParseStatements().visit(cu, null);
+
 
     }
-
-   
-    private static class MethodGenericTest extends GenericVisitorAdapter{
-        
-      //whuuuut? genericvisitoradapat might store blocks in a list by defaul... 'might'"
-       
-    }
-   
     
     /**
-     * visiting methods using VoidVisitorAdapter parent class.
+     * Visit every method to parse statements from each
      */
-    private static class MethodVisitorTest extends VoidVisitorAdapter {
+    private static class ParseStatements extends VoidVisitorAdapter {
+       
 
-        
-//        public static void getAll_Methods(MethodDeclaration n, Object arg){
-//             BlockStmt list_of_methods = new BlockStmt();
-//             
-//        }
         
         @Override
         public void visit(MethodDeclaration n, Object arg) {
             
-//            System.out.println(n.getName());//Gets method names.
+
 //            System.out.println(n.getBody().getChildrenNodes());//Gets contents of method.
 
-            List<Statement> statements;
-             BlockStmt block = new BlockStmt();
-            
-            
-//             
-//             List<IfStmt> if_statements_list;
-//            List<ForStmt> for_statements_list;
-//            IfStmt instance_IF = new IfStmt();
-//            ForStmt instance_FOR = new ForStmt();
 
-            //Test to see if we can get contents of method and set them in a block.
+//             BlockStmt block = new BlockStmt();
+            
+            
+
+            //Get every method's name from the parsed .java input stream
             System.out.println("----------------Statements in [" + n.getName() + "] are: ------------------------------------------------------------------");
           
-        
+  
+            //every method declaration contains finite number of statments (lines of code)
+            LIST_STATEMENTS = n.getBody().getStmts();
             
             
-            block = n.getBody();
             
             
-            statements = block.getStmts();
-            
-            
-            int x =1;
-            for(Statement statement: statements){
+            for(Statement statement: LIST_STATEMENTS){
 
-                System.out.println("{"+ x + "} ");
+                System.out.println("{"+ statmentID + "} ");
                 
-//                if(statement instanceof IfStmt)
-//                {
-//                    System.out.println("This is an if_stmt!!->  "+statement.getChildrenNodes());
-//                }
-                System.out.println(statement.getChildrenNodes());
+                if(statement instanceof IfStmt){
+                    
+                    visit((IfStmt) statement,null);
+                }
+                else if(statement instanceof ForStmt){
+                    
+                    visit((ForStmt) statement,null);
+                    
+                }
+                else if(statement instanceof WhileStmt){
+                    
+                    visit((WhileStmt) statement,null);
+                }
+                else if(statement instanceof BreakStmt){
+                    
+                    visit((BreakStmt) statement,null);
+                    
+                }
+                else{
+                     System.out.println(statement);
+                }
+              
+               
                 
-                x++;
+                statmentID++;
             }
             System.out.println();
             
@@ -122,10 +123,10 @@ public class CodeGraph
 
         }
         
-
+      
         
         @Override
-        public void visit(IfStmt n, Object org){
+        public void visit(IfStmt n, Object arg){
            
             if(n instanceof IfStmt){
                 System.out.println("IF-STMT at line ("+n.getBeginLine()+") --> "+n.getCondition());
@@ -137,26 +138,25 @@ public class CodeGraph
                 }
                 System.out.println("IF-STMT ENDS at line ("+n.getEndLine()+")");
             }
-            super.visit(n,org);
+            super.visit(n, arg);
             
         }
         
         @Override
-        public void visit(ForStmt n, Object org){
+        public void visit(ForStmt n, Object arg){
             
              if(n instanceof ForStmt){
                  
-                System.out.println("FOR-LOOP is at line--> ("+n.getBeginLine()+") --> "+n.getCompare());
+                System.out.println("FOR-LOOP at line--> ("+n.getBeginLine()+") --> "+n.getCompare());
                 System.out.println("FOR-LOOP BODY "+n.getBody());
                 System.out.println("FOR-LOOP ENDS at line ("+n.getEndLine()+")");
              }
-            
-            super.visit(n,org);
+               super.visit(n, arg);
             
         }
         
          @Override
-        public void visit(WhileStmt n, Object org){
+        public void visit(WhileStmt n, Object arg){
             
             if(n instanceof WhileStmt){
             System.out.println("WHILE-LOOP is --> ("+n.getBeginLine()+") --> "+n.getCondition());
@@ -164,41 +164,43 @@ public class CodeGraph
             System.out.println("WHILE-LOOP ENDS at line ("+n.getEndLine()+")");
             }
             
-            super.visit(n,org);
+            super.visit(n, arg);
             
         }
         
          @Override
-        public void visit(BreakStmt n, Object org){
+        public void visit(BreakStmt n, Object arg){
             
             if(n instanceof BreakStmt){
             System.out.println("BREAK! is --> ("+n.getBeginLine()+") --> ID="+n.getId());
            
             }
-            
-            super.visit(n,org);
-            
+         
+               super.visit(n, arg);
         }
         
         
         
         
-             //ignore this funciton for now, it might be usfule to read every single word in the code
-        public static void process(Node node){
-            System.out.println("Trigger process funtion!!!!!");
-            //this loop get a node, (a node is also know as a statment)
-            for(Node child : node.getChildrenNodes()){
-                System.out.println("My child is-----> "+child);
-                process(child);
-            }
-            
-            System.out.println("BEGIN:  "+node.getBeginLine());
-             System.out.println("END:   "+node.getEndLine());
-              System.out.println("PARENT NODE:   "+node.getParentNode());
-              System.out.println("-----");
-        }
-
+//             //ignore this funciton for now, it might be usfule to read every single word in the code
+//        public static void process(Node node){
+//            System.out.println("Trigger process funtion!!!!!");
+//            //this loop get a node, (a node is also know as a statment)
+//            for(Node child : node.getChildrenNodes()){
+//                System.out.println("My child is-----> "+child);
+//                process(child);
+//            }
+//            
+//            System.out.println("BEGIN:  "+node.getBeginLine());
+//             System.out.println("END:   "+node.getEndLine());
+//              System.out.println("PARENT NODE:   "+node.getParentNode());
+//              System.out.println("-----");
+//        }
+        
+    
+ 
         
     }
+    
 
 }
