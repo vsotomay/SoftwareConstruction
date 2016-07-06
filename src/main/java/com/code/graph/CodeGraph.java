32a7115261d02c6b232f05e915465711480db162
Julio <jmcorral2@miners.utep.edu>
2016-07-05 20:42:27 -0600

    Update CodeGraph.java

--- src/main/java/com/code/graph/CodeGraph.java
+++ src/main/java/com/code/graph/CodeGraph.java
@@ -27,9 +27,11 @@ import org.apache.commons.lang3.StringEscapeUtils;
 public class CodeGraph
 {
     
-    static  List<Statement> LIST_STATEMENTS;
+    static List<Statement> LIST_STATEMENTS;
     //count statments from the .java input code
-    static int statmentID =1;
+    static int statment_Number =1;
+    
+    
     
     public static void main( String[] args ) throws IOException, ParseException {
 
@@ -47,20 +49,20 @@ public class CodeGraph
             in.close();
         }
 
-        
-
-//        System.out.println(cu.toString());
-        System.out.println("Visiting Methods in QuickSort.java");
+   
+        System.out.println("Visiting Statments");
         System.out.println();
-        new ParseStatements().visit(cu, null);
+        new VisitStatements().visit(cu, null);
 
-
+//        System.out.println("ey there "+LIST_STATEMENTS.toString());
     }
+    
+
     
     /**
      * Visit every method to parse statements from each
      */
-    private static class ParseStatements extends VoidVisitorAdapter {
+    private static class VisitStatements extends VoidVisitorAdapter {
        
 
         
@@ -72,22 +74,18 @@ public class CodeGraph
 
 
 //             BlockStmt block = new BlockStmt();
-            
-            
 
-            //Get every method's name from the parsed .java input stream
+            //Get every method's name from the parsed .java file
             System.out.println("----------------Statements in [" + n.getName() + "] are: ------------------------------------------------------------------");
           
   
             //every method declaration contains finite number of statments (lines of code)
             LIST_STATEMENTS = n.getBody().getStmts();
-            
-            
-            
+          
             
             for(Statement statement: LIST_STATEMENTS){
 
-                System.out.println("{"+ statmentID + "} ");
+                System.out.println("stmtNum : {{"+ statment_Number + "}} ");
                 
                 if(statement instanceof IfStmt){
                     
@@ -96,6 +94,7 @@ public class CodeGraph
                 else if(statement instanceof ForStmt){
                     
                     visit((ForStmt) statement,null);
+       
                     
                 }
                 else if(statement instanceof WhileStmt){
@@ -108,28 +107,24 @@ public class CodeGraph
                     
                 }
                 else{
-                     System.out.println(statement);
+                     System.out.println("Line "+statement.getBeginLine()+" : "+statement);
+                    
                 }
               
                
                 
-                statmentID++;
+                statment_Number++;
             }
-            System.out.println();
-            
-           
-             
-            super.visit(n, arg);
-
+            System.out.println("---------Method [" + n.getName() + "] Ends------------------------------------------------------------------------------------");
+   
         }
-        
-      
+          
         
         @Override
         public void visit(IfStmt n, Object arg){
            
             if(n instanceof IfStmt){
-                System.out.println("IF-STMT at line ("+n.getBeginLine()+") --> "+n.getCondition());
+                System.out.println("IF-STMT at line : ("+n.getBeginLine()+") --> "+n.getCondition());
                 if(n.getThenStmt() != null){
                      System.out.println("THEN---STMT at line ("+n.getThenStmt().getBeginLine()+") --> "+n.getThenStmt());
                 }
@@ -138,7 +133,9 @@ public class CodeGraph
                 }
                 System.out.println("IF-STMT ENDS at line ("+n.getEndLine()+")");
             }
-            super.visit(n, arg);
+            
+            //flag visited methods using voidvisitoradapter
+            super.visit(n, arg); 
             
         }
         
@@ -147,7 +144,7 @@ public class CodeGraph
             
              if(n instanceof ForStmt){
                  
-                System.out.println("FOR-LOOP at line--> ("+n.getBeginLine()+") --> "+n.getCompare());
+                System.out.println("FOR-LOOP at line : ("+n.getBeginLine()+") --> "+n.getCompare());
                 System.out.println("FOR-LOOP BODY "+n.getBody());
                 System.out.println("FOR-LOOP ENDS at line ("+n.getEndLine()+")");
              }
@@ -159,7 +156,7 @@ public class CodeGraph
         public void visit(WhileStmt n, Object arg){
             
             if(n instanceof WhileStmt){
-            System.out.println("WHILE-LOOP is --> ("+n.getBeginLine()+") --> "+n.getCondition());
+            System.out.println("WHILE-LOOP at line : ("+n.getBeginLine()+") --> "+n.getCondition());
             System.out.println("WHILE-LOOP BODY is --> "+n.getBody());
             System.out.println("WHILE-LOOP ENDS at line ("+n.getEndLine()+")");
             }
@@ -172,35 +169,14 @@ public class CodeGraph
         public void visit(BreakStmt n, Object arg){
             
             if(n instanceof BreakStmt){
-            System.out.println("BREAK! is --> ("+n.getBeginLine()+") --> ID="+n.getId());
+                n.setId("0");
+            System.out.println("BREAK! at line : ("+n.getBeginLine()+") --> ID="+n.getId());
            
             }
          
                super.visit(n, arg);
         }
-        
-        
-        
-        
-//             //ignore this funciton for now, it might be usfule to read every single word in the code
-//        public static void process(Node node){
-//            System.out.println("Trigger process funtion!!!!!");
-//            //this loop get a node, (a node is also know as a statment)
-//            for(Node child : node.getChildrenNodes()){
-//                System.out.println("My child is-----> "+child);
-//                process(child);
-//            }
-//            
-//            System.out.println("BEGIN:  "+node.getBeginLine());
-//             System.out.println("END:   "+node.getEndLine());
-//              System.out.println("PARENT NODE:   "+node.getParentNode());
-//              System.out.println("-----");
-//        }
-        
     
- 
-        
     }
     
-
 }
