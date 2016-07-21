8b1091b8f81ba14b6ef44abb51e7cbc7e667530d
Julio <jmcorral2@miners.utep.edu>
2016-07-20 21:41:47 -0600

    Update and rename QuickSort.java to Sample.java

---
+++ Sample.java
@@ -0,0 +1,55 @@
+/*
+ 
+ Quick Sort k-largest elements */
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Sample
+{
+    
+    
+    public static void main (String [] args)
+    {
+        Scanner console = new Scanner(System.in);
+        
+        
+        
+        int arraysize=console.nextInt();
+        
+        //global variable in the size of the array prompted by the user
+        final int max=arraysize;
+        
+        
+        
+        
+         for(int index=first+1;index<=last;index++){
+            //swap if the index is less than the current pivot
+            for(int index=first+1;index1<=last1;index++)
+            {
+                System.out.println("1111inner for loop its body");
+            }
+             for(int index=first+1;index2<=last2;index++)
+            {
+                System.out.println("222222inner for loop its body");
+            }
+          
+        }
+
+       
+  //  for(int index=first+1;index3<=last3;index++)
+            // {
+            //     System.out.println("33333ainner for loop its body");
+            // }
+     
+
+    }
+    
+    
+    
+    
+    
+    
+   
+    
+}
